36038f01add705c337bdc5d48478469f373ca3b5
Bry <bruhyeun@msn.com>
2017-12-25 15:24:59 +0000

    Add navbar helpers.

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,62 @@
 module StaticPagesHelper
+  
+  # Adds a link to the navbar
+  def nav_item(item, path)
+    content_tag :li, class: "nav-item" do
+      link_to item, path, class: "nav-link"
+    end
+  end
+  
+  # Adds an icon link to the navbar
+  def nav_icon(icon, path)
+    content_tag :li, class: "nav-item p-0" do
+      link_to eval("mi.#{icon}"), path, class: "nav-link"
+    end
+  end
+    
+  # Adds a dropdown link to the navbar
+  def dropdown(item, path, &block)
+    content_tag :li, class: "nav-item dropdown" do
+      (capture(&block) if block_given?) <<
+      (link_to item, path, class: "nav-link",
+                          id:"navbarDropdown", 
+                          data: { toggle: "dropdown" }, 
+                          aria: { haspopup: true, expanded: false })
+    end
+  end
+
+  # Adds a dropdown icon link to the navbar
+  def dropdown_icon(icon, path, &block)
+    content_tag :li, class: "nav-item dropdown" do
+      (capture(&block) if block_given?) <<
+      (link_to eval("mi.#{icon}"), path, class: "nav-link",
+                          id:"navbarDropdown", 
+                          data: { toggle: "dropdown" }, 
+                          aria: { haspopup: true, expanded: false })
+    end
+  end
+  
+  # Adds a dropdown menu to the dropdown link
+  def dropdown_menu(class_option="dropdown-menu",&block)
+    content_tag :div, class: class_option, 
+                      aria: { labelledby: "navbarDropdown" } do
+      capture(&block) if block_given?
+    end
+  end
+  
+  # Adds a dropdown link to the dropdown menu
+  def dropdown_item(item, path)
+    link_to item, path, class: "dropdown-item"
+  end
+  
+  # Adds a dropdown icon link to the dropdown menu
+  def dropdown_item_icon(icon, path, label)
+    link_to eval("mi.#{icon}").css_class('center-vertically').to_s << 
+            " #{label}", path, class: "dropdown-item"
+  end
+  
+  # Adds a divider to the dropdown menu
+  def dropdown_divider
+    content_tag :div, "", class: "dropdown-divider"
+  end
 end
